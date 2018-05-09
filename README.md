1b88976d08f38cf1bec222a4d8351daf8e7646f0
Filip Stangricki <stangricki@gmail.com>
2018-05-09 15:03:33 +0200

    Added NPM packages to gulpfile.js and made some testing

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # simple-webdev-starter-pack
+
+NPM used (at the moment):
+
+
+    "autoprefixer": "^8.4.1",
+    "browser-sync": "^2.24.4",
+    "gulp": "^3.9.1",
+    "gulp-cssnano": "^2.1.3",
+    "gulp-postcss": "^7.0.1",
+    "gulp-sass": "^4.0.1",
+    "gulp-watch": "^5.0.0",
+    "postcss-hexrgba": "^1.0.1"
--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,58 @@
-require('./gulp/tasks/watch');
+// require('./gulp/tasks/watch');
+
+const gulp = require('gulp'),
+	  sass = require('gulp-sass'),
+	  postcss = require('gulp-postcss'),
+	  autoprefixer = require('autoprefixer'),
+	  hexrgba = require('postcss-hexrgba'),
+	  watch = require('gulp-watch'),
+	  cssnano = require('gulp-cssnano'),
+	  browserSync = require('browser-sync').create();
+
+/* PATHS */
+
+const STYLES_DEV_SASS_DIR = './app/assets/scss/**/*.scss',
+	  STYLES_TEMP_CSS_DIR = './app/temp/styles/',
+	  INDEX_HTML_FILE = './app/index.html';
+
+/****************DEFAULT*******************/
+gulp.task('default', function(){
+	console.log("Default task started!")
+})
 
 
+/****************STYLES*******************/
+gulp.task('styles', function(){
+	gulp.src(STYLES_DEV_SASS_DIR)
+		.pipe(sass())
+		.pipe(postcss([autoprefixer, hexrgba]))
+		.on('error', function(errorInfo){
+			console.log(errorInfo.toString());
+			this.emit('end')
+		})
+		 // .pipe(cssnano()) // uncomment to minify CSS file
+		.pipe(gulp.dest(STYLES_TEMP_CSS_DIR))
+})
 
+/****************WATCH*******************/
+gulp.task('watch', function(){
 
+	browserSync.init({
+		notify: false,
+        server: {
+            baseDir: "./app"
+        }
+    });
+
+	watch(STYLES_DEV_SASS_DIR, function(){
+		gulp.start('styles');
+	})
+
+	watch(INDEX_HTML_FILE, function(){
+		browserSync.reload();
+	})
+
+	watch(STYLES_TEMP_CSS_DIR, function(){
+		browserSync.reload();
+	})
+})
